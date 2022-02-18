0029faef1b396058b784599fdef6ff03f7da9a8c
Jérémy La Scala <jeremy.lascala@epfl.ch>
2022-02-18 12:05:11 +0100

    chore: clean and prepare FinalViewFooter.js for port

--- src/components/main/note/final-view/FinalViewFooter.js
+++ src/components/main/note/final-view/FinalViewFooter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from 'react-i18next';
 import Typography from '@material-ui/core/Typography';
@@ -14,8 +14,8 @@ const FinalViewFooter = ({ userId }) => {
   const classes = useStyles();
   const { t } = useTranslation();
 
-  const standalone = useSelector(({ context }) => context.standalone);
-  const usersArray = useSelector(({ users }) => users.content);
+  // const standalone = useSelector(({ context }) => context.standalone);
+  const usersArray = useSelector(({ users }) => users.content); // TODO: Port this
 
   // if app is accessed standalone, don't show any information in the footer (where user info is displayed)
   // a typography element is still rendered for visual consistency

12c5e7386eece7c46c3f97bb35388a5698f769e8
Jeremy Jayet <jeremy.jayet@protonmail.ch>
2022-03-09 12:06:24 +0100

    chore: cleaning settings

--- src/config/settings.js
+++ src/config/settings.js
@@ -2,11 +2,7 @@ import { LOCAL_API_HOST } from './api';
 import { CONTEXTS } from './contexts';
 import { REACT_APP_MOCK_API } from './env';
 
-export const MAX_INPUT_LENGTH = 5000;
-export const MAX_ROWS = 10;
 export const DEFAULT_LANG = 'en';
-export const DEFAULT_MODE = 'student';
-export const TEACHER_MODE = 'teacher';
 
 // TODO: Delete.
 // avoid breaking the app in production when embedded in different contexts
@@ -22,17 +18,7 @@ try {
 
 export const DEFAULT_API_HOST = defaultApiHost;
 
-export const MODES = {
-  TEACHER: 'teacher',
-  STUDENT: 'student',
-  PRODUCER: 'producer',
-  EDUCATOR: 'educator',
-  ADMIN: 'admin',
-  CONSUMER: 'consumer',
-  LEARNER: 'learner',
-};
-
-// todo: use from graasp constants
+// TODO: use from graasp constants
 export const PERMISSION_LEVELS = {
   WRITE: 'write',
   READ: 'read',
@@ -40,15 +26,6 @@ export const PERMISSION_LEVELS = {
 };
 
 export const DEFAULT_PERMISSION = 'read';
-
-// we haven't decided what to call the teacher mode
-export const TEACHER_MODES = [TEACHER_MODE, 'producer', 'educator', 'admin'];
-
-export const PUBLIC_VISIBILITY = 'public';
-export const DEFAULT_VISIBILITY = PUBLIC_VISIBILITY;
-
-export const PUBLIC_OWNERSHIP = 'all';
-export const DEFAULT_OWNERSHIP = PUBLIC_OWNERSHIP;
 
 export const MAX_NUM_FILES = 1;
 export const MAX_FILE_SIZE = 5 * 1000 * 1000;

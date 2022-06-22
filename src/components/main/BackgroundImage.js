94a3969f3237b3bb3ecce3f9e64c45f2f9da8e03
Jérémy La Scala <jeremy.lascala@epfl.ch>
2022-06-22 16:56:28 +0200

    fix: background image

--- src/components/main/BackgroundImage.js
+++ src/components/main/BackgroundImage.js
@@ -1,7 +1,8 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+
+import React, { useEffect, useState } from 'react';
 import { PropTypes } from 'prop-types';
-import { Image } from 'react-konva';
-import { useImage } from 'react-konva-utils';
+import { Image as ImageK } from 'react-konva';
 import { APP_SETTINGS } from '../../constants/constants';
 import { useAppSettingFile, useAppSettings } from '../context/appData';
 
@@ -18,12 +19,28 @@ const BackgroundImage = ({ x, y }) => {
     ),
   );
 
-  let image;
+//   const [blob, setBlob] = useState(null);
 
-  if(backgroundImage){
-    const url = URL.createObjectURL(backgroundImage);
-    [image] = useImage(url);
-  }
+//   const fileUrl = 'https://picsum.photos/200/300';
+
+//   useEffect(() => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.responseType = 'blob';
+//   xhr.onload = () => {
+//     setBlob(xhr.response); // xhr.response is a blob
+//   };
+//   xhr.open('GET', fileUrl);
+//   xhr.send();
+// }, []);
+
+  const image = new Image();
+
+  useEffect(() => {
+    if (backgroundImage) {
+      const url = URL.createObjectURL(backgroundImage);
+      image.src = url;
+    }
+  }, [backgroundImage]);
 
   // const [image, setImage] = useState(null);
 
@@ -40,7 +57,7 @@ const BackgroundImage = ({ x, y }) => {
   }
 
   return (
-    <Image
+    <ImageK
       x={x - (image?.width ?? 0) / 2}
       y={y - (image?.height ?? 0) / 2}
       image={image}

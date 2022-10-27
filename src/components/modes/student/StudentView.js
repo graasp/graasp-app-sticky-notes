4c31639777a7f02a3bb4fd517c8e00908e234bfa
Jérémy La Scala <jeremy.lascala@epfl.ch>
2022-10-27 11:26:14 +0200

    refactor: remove withTranslation in views

--- src/components/modes/student/StudentView.js
+++ src/components/modes/student/StudentView.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { withTranslation } from 'react-i18next';
 import Canvas from '../../main/Canvas';
 
 const StudentView = () => <Canvas />;
 
-export default withTranslation()(StudentView);
+export default StudentView;
--- src/components/modes/teacher/TeacherView.js
+++ src/components/modes/teacher/TeacherView.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { withTranslation } from 'react-i18next';
 import Canvas from '../../main/Canvas';
 
 const TeacherView = () => <Canvas />;
 
-export default withTranslation()(TeacherView);
+export default TeacherView;

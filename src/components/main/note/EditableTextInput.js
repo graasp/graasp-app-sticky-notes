7f9bbc6b40486dacb0b9f277f0aeba717531490d
Jérémy La Scala <jeremy.lascala@epfl.ch>
2022-11-04 09:50:11 +0100

    feat: temporary validate button
    Not completely implemented

--- src/components/main/note/EditableTextInput.js
+++ src/components/main/note/EditableTextInput.js
@@ -1,8 +1,12 @@
 import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import DoneIcon from '@mui/icons-material/Done'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
 import PropTypes from 'prop-types';
 import './note_style.css';
+import { Container } from '@mui/material';
 
 const EditableTextInput = forwardRef(({ value, onChange, onKeyDown }, ref) => {
   const textInput = useRef();
@@ -35,15 +39,22 @@ const EditableTextInput = forwardRef(({ value, onChange, onKeyDown }, ref) => {
   }));
 
   return (
-    <ReactQuill
-      theme="bubble"
-      value={value}
-      onChange={onChange}
-      onKeyDown={onKeyDown}
-      ref={textInput}
-      modules={modules}
-      formats={formats}
-    />
+    <Stack direction="row" justifyContent='space-between'>
+      <ReactQuill
+        theme="bubble"
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        ref={textInput}
+        modules={modules}
+        formats={formats}
+      />
+      <Container>
+      <IconButton>
+        <DoneIcon />
+      </IconButton>
+      </Container>
+    </Stack>
   );
 });
 

98a03a051974f3ce381c5e66762ca0153353cb03
Jérémy La Scala <jeremy.lascala@epfl.ch>
2023-02-16 18:04:28 +0100

    refactor(UI): replace slider by zoom buttons

--- src/components/main/CanvasScaleControl.tsx
+++ src/components/main/CanvasScaleControl.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import ZoomInIcon from '@mui/icons-material/ZoomIn';
+import ZoomOutIcon from '@mui/icons-material/ZoomOut';
 import { styled } from '@mui/material';
-import Slider from '@mui/material/Slider';
-import Typography from '@mui/material/Typography';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 
 import { ZOOM_MAX, ZOOM_MIN, ZOOM_STEP } from '../../config/settings';
 
@@ -31,30 +34,39 @@ const CanvasScaleControl = (
   const { canvasScale, setCanvasScale } = props;
   const { t } = useTranslation();
 
-  const getAriaLabel = (val: number): string => `${100 * val}%`;
+  const getAriaLabel = (): string => `${Math.floor(100 * canvasScale)}%`;
 
-  const handleChange = (_event: Event, value: number | number[]): void => {
-    if (typeof value === 'number') {
-      setCanvasScale(value);
-    } else {
-      setCanvasScale(value[0]);
-    }
+  const zoomIn = (): void => {
+    setCanvasScale(Math.max(canvasScale + ZOOM_STEP, ZOOM_MAX));
+  };
+  const zoomOut = (): void => {
+    setCanvasScale(Math.max(canvasScale - ZOOM_STEP, ZOOM_MIN));
   };
   return (
     <MainContainer>
-      <Typography>{t('Zoom')}</Typography>
-      <Slider
-        sx={{ width: '80% ' }}
-        getAriaLabel={getAriaLabel}
-        defaultValue={1}
-        value={canvasScale}
-        onChange={handleChange}
-        valueLabelDisplay="auto"
-        step={ZOOM_STEP}
-        marks
-        min={ZOOM_MIN}
-        max={ZOOM_MAX}
-      />
+      <Tooltip title={`${getAriaLabel()} zoom`}>
+        <ButtonGroup
+          color="primary"
+          variant="contained"
+          size="large"
+          aria-label={getAriaLabel()}
+        >
+          <IconButton
+            color="primary"
+            aria-label={t('ZOOM_IN')}
+            onClick={zoomIn}
+          >
+            <ZoomInIcon />
+          </IconButton>
+          <IconButton
+            color="primary"
+            aria-label={t('ZOOM_OUT')}
+            onClick={zoomOut}
+          >
+            <ZoomOutIcon />
+          </IconButton>
+        </ButtonGroup>
+      </Tooltip>
     </MainContainer>
   );
 };
